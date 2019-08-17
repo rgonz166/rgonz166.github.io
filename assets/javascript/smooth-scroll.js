55ca92d82996f22b344e92ca0f4aa5d4d2b200a5
Raul Gonzalez <rgonz166@gmail.com>
2019-08-16 23:51:46 -0700

    removed everything starting from scratch

--- assets/javascript/smooth-scroll.js
+++
@@ -1,6 +0,0 @@
-// This javascript application adds smooth scroll to our site
-var scroll = SmoothScroll('.navbar a[href*="#"]',{
-    speed:800,
-    speedAsDuration: true,
-    easing: 'easeInOutCubic'
-})
