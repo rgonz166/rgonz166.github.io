126592a97222313ccc9dfc6255a24cd8612a4208
Raul Gonzalez <rgonz166@gmail.com>
2019-08-24 07:08:35 -0700

    added cards for projects, added responsiveness

--- assets/javascript/smooth-scroll.js
+++ assets/javascript/smooth-scroll.js
@@ -1,12 +1,7 @@
 // Starts smooth scroll on click
-// $('.navbar a').on('click',function(e){
-//     e.preventDefault();
-//     var hash = this.hash;
 
-//     $('html, body').animate({
-//         scrollTop: $(hash).offset().top
-//     },300);
-// })
-
-
-var scroll = new SmoothScroll('.navbar a[href*="#"]',{speed: 800});
+var scroll = new SmoothScroll('.navbar a[href*="#"]',
+{
+    speed: 800,
+    speedAsDuration:true
+});
