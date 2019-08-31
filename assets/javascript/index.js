d237c5b4b40ccca2e36bf45b489e575c8583adff
Raul Gonzalez <rgonz166@gmail.com>
2019-08-30 20:43:02 -0700

    New form that actually works

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,11 +1,4 @@
-// $(document).ready(function(){
-//     $(".container-fluid h1").fadeIn(1000);
-//     setTimeout(function(){
-//         $(".container-fluid p").fadeIn(1000);
-//     },1000);
-    
 
-// });
 $(document).ready(function(){
     $('.appear h1').attr('class','show');
     setTimeout(function(){
